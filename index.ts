397de55e43e70c5837c7cf26df84fcb9dfc8b064
BucosAlexandru <100204467+BucosAlexandru@users.noreply.github.com>
2025-07-08 16:49:43 +0300

    Refactor: eliminare cheie Stripe din cod

--- index.ts
+++ index.ts
@@ -1,7 +1,9 @@
 import { serve } from "https://deno.land/std@0.168.0/http/server.ts";
 import Stripe from "npm:stripe";
 
-const stripe = new Stripe(Deno.env.get("STRIPE_SECRET_KEY"), { apiVersion: "2022-11-15" });
+const stripe = new Stripe(Deno.env.get("STRIPE_SECRET_KEY"), {
+  apiVersion: "2022-11-15",
+});
 
 serve(async (req) => {
   try {
