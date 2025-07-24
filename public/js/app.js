830084e105a79ff1daf6d73515434cc134ffba6f
BucosAlexandru <100204467+BucosAlexandru@users.noreply.github.com>
2025-07-24 15:48:57 +0300

    Rename pdf export

--- public/js/app.js
+++ public/js/app.js
@@ -531,7 +531,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     html2pdf().set({
       margin: 0.5,
-      filename: 'lista-cumparaturi.pdf',
+      filename: 'meal-planner.pdf',
       image: { type: 'jpeg', quality: 0.98 },
       html2canvas: { scale: 2 },
       jsPDF: { unit: 'in', format: 'a4', orientation: 'portrait' }
